bfca1990e07b098cf9156b41ba01d48d0a36c46c
albaqirhidzqi <skadimoon74@gmail.com>
2025-03-21 07:58:07 +0700

    adding data WarhouseName and SalesTeamName from seeds to sales_data_stg

--- dbt_hidzqi/models/staging/sales_data_stg.sql
+++ dbt_hidzqi/models/staging/sales_data_stg.sql
@@ -1,13 +1,15 @@
 SELECT
     OrderNumber,
     SalesChannel,
-    WarehouseCode,
+    SL.WarehouseCode,
+    WarehouseName,
     STR_TO_DATE(ProcuredDate, "%d/%m/%y") ProcuredDate,
     STR_TO_DATE(OrderDate, "%d/%m/%y") OrderDate,
     STR_TO_DATE(ShipDate, "%d/%m/%y") ShipDate,
     STR_TO_DATE(DeliveryDate, "%d/%m/%y") DeliveryDate,
     CurrencyCode,
-    SalesTeamID,
+    SL.SalesTeamID,
+    SalesTeamName,
     CustomerID,
     StoreID,
     ProductID,
@@ -16,4 +18,8 @@ SELECT
     CAST(SUBSTRING(UnitCost,2) AS DOUBLE) UnitCost,
     CAST(SUBSTRING(UnitPrice,2) AS DOUBLE) UnitPrice
 FROM 
-    {{ source('sale', 'sales') }}
+    {{ source('sale', 'sales') }} SL
+LEFT JOIN {{ ref("sales_team_with_names")}} ST
+    ON SL.SalesTeamID = ST.SalesTeamID
+LEFT join {{ ref('warehouse_with_names')}} WH
+    ON SL.WarehouseCode = WH.WarehouseCode
